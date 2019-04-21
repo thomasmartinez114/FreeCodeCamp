2a29c849bfdfcf8a628045c7500426720cc2ccd7
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-21 20:50:33 +0000

    completed 02

---
+++ javascript/Basic Algorithm Scripting/02reverseAString.js
@@ -0,0 +1,9 @@
+// Reverse a String
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
+reverseString("Howdy");
+reverseString("Greetings from Earth");
