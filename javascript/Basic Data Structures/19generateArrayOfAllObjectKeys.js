f7dfb00ff759cf6e26ced5f1af94820461ed082d
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-04-22 04:42:40 +0000

    completed 19

--- javascript/Basic Data Structures/19generateArrayOfAllObjectKeys.js
+++ javascript/Basic Data Structures/19generateArrayOfAllObjectKeys.js
@@ -1,2 +1,28 @@
 // Generate an Array of All Object Keys with Object.keys()
 
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+  return Object.keys(obj);
+  // change code above this line
+}
+
+console.log(getArrayOfUsers(users));
