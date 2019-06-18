7d38c87308fe91d547fbc0b240b4d318735fa2b7
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-06-18 18:04:46 -0400

    completed 02diffTwoArrays

--- javascript/Intermediate Algorithm Scripting/02diffTwoArrays.js
+++ javascript/Intermediate Algorithm Scripting/02diffTwoArrays.js
@@ -6,3 +6,31 @@
 // Note
 // You can return the array with its elements in any order.
 
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // Same, same; but different.
+
+  // Iterate through arr1
+  for (let i = 0; i < arr1.length; i ++){
+
+    // Check if the element is in arr2
+    if (arr2.indexOf(arr1[i]) === -1){
+
+      // Store the element in newArr
+      newArr.push(arr1[i])
+    }
+  }
+
+  // Iterate through arr2
+  for (let i = 0; i < arr2.length; i ++) {
+
+  // Check if the element is in arr1
+    if (arr1.indexOf(arr2[i]) === -1){
+
+  // Store the element in newArr
+    newArr.push(arr2[i])
+    }
+  }
+
+  return newArr;
+}
