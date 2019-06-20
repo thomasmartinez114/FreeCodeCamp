8950bb44ceeb22ccf38dd7b5ae49534bf73e8c48
Thomas Martinez <thomasmartinez.student@careerdevs.com>
2019-06-20 17:58:08 -0400

    completed 05

--- javascript/Basic Algorithm Scripting/05returnLargestNumbers.js
+++ javascript/Basic Algorithm Scripting/05returnLargestNumbers.js
@@ -4,3 +4,15 @@
 
 // Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
 
+function largestOfFour(arr) {
+  // You can do this!
+  var result = [];
+
+  for (var i = 0; i < arr.length; i++){
+    // console.log(Math.max(...arr[i]))
+    result.push(Math.max(...arr[i]))
+  }
+  return result
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
