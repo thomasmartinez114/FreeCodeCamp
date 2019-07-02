44442de2c13013486e13f54959138c5460ed34f3
unknown <tmartinez114@gmail.com>
2019-07-02 14:20:50 +0200

    corrected error in exercises.js

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -11,7 +11,7 @@ router.route("/add").post((req, res) => {
   const username = req.body.username;
   const description = req.body.description;
   const duration = Number(req.body.duration);
-  const data = Data.parse(req.body.date);
+  const date = Date.parse(req.body.date);
 
   const newExercise = new Exercise({
     username,
