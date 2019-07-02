e3f3554dc4ae81ecc7f2c7514e572af2530192ae
unknown <tmartinez114@gmail.com>
2019-07-02 14:34:25 +0200

    corrected typo

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 let Exercise = require("../models/exercise.model");
 
-router.router("/").get((req, res) => {
+router.route("/").get((req, res) => {
   Exercise.find()
     .then(exercises => res.json(exercises))
     .catch(err => res.status(400).json("Error: " + err));
