aade39181310e309bb7c1a3cc32a01c6175b156d
unknown <tmartinez114@gmail.com>
2019-07-02 13:57:41 +0200

    added module.exports

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -19,4 +19,11 @@ router.route("/add").post((req, res) => {
     duration,
     date
   });
+
+  newExercise
+    .save()
+    .then(() => res.json("Exercise added!"))
+    .catch(err => res.status(400).json("Error: " + err));
 });
+
+module.exports = router;
