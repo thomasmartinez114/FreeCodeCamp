4960a03d3ca186a92ea64782a6c382ced74538bf
unknown <tmartinez114@gmail.com>
2019-07-03 18:36:54 +0200

    added route for :id in exercise.js

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -26,4 +26,10 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
 module.exports = router;
