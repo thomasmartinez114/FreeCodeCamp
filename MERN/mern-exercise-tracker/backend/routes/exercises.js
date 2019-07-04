92c3e2a791335b1f4a907aec61907b6115dae58b
unknown <tmartinez114@gmail.com>
2019-07-04 12:04:43 +0200

    added update/:id to exercise.js

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -39,11 +39,19 @@ router.route("/:id").delete((req, res) => {
 });
 
 router.route("/update/:id").post((req, res) => {
-  Exercise.findById(req.params.id).then(exercise => {
-    exercise.username = req.body.username;
-    exercise.description = req.body.description;
-    exercise.duration = Number(req.body.duration);
-  });
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise updated!"))
+        .catch(err => res.status(400).json("Error: " + err));
+    })
+    .catch(err => res.status(400).json("Error: " + err));
 });
 
 module.exports = router;
