9675ba113ebbb688f4b79887c7f9ef0a36fc1d01
unknown <tmartinez114@gmail.com>
2019-07-04 11:53:34 +0200

    added delete route for /:id

--- MERN/mern-exercise-tracker/backend/routes/exercises.js
+++ MERN/mern-exercise-tracker/backend/routes/exercises.js
@@ -32,4 +32,18 @@ router.route("/:id").get((req, res) => {
     .catch(err => res.status(400).json("Error: " + err));
 });
 
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise deleted."))
+    .catch(err => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id).then(exercise => {
+    exercise.username = req.body.username;
+    exercise.description = req.body.description;
+    exercise.duration = Number(req.body.duration);
+  });
+});
+
 module.exports = router;
