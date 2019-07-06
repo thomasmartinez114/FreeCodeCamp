09e6c438fba326f0c8b4222971d1d1c43b48030e
unknown <tmartinez114@gmail.com>
2019-07-06 15:36:59 +0200

    added NavBar and Routes in app.js

--- MERN/mern-exercise-tracker/src/App.js
+++ MERN/mern-exercise-tracker/src/App.js
@@ -3,7 +3,16 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="Container">Hello World!</div>;
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={ExercisesList} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
 }
 
 export default App;
