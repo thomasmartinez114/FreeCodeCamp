42171af77a565b700104fac1ea7b9553c66deaa1
unknown <tmartinez114@gmail.com>
2019-07-07 18:41:37 +0200

    resolved compile error from mispelt paths and component titles

--- MERN/mern-exercise-tracker/src/components/edit-exercise.component.js
+++ MERN/mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class EditExercises extends Component {
+export default class EditExercise extends Component {
   render() {
     return (
       <div>
