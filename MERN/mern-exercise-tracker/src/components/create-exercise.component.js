9aabed7a3cda665b8eb39af7025ed67edc878559
unknown <tmartinez114@gmail.com>
2019-07-07 19:01:22 +0200

    created props in create-exercise

--- MERN/mern-exercise-tracker/src/components/create-exercise.component.js
+++ MERN/mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: []
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
