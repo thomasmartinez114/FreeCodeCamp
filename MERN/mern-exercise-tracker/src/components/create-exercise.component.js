3246a9d49966f66d92af7a3b8017071ec41a6d47
unknown <tmartinez114@gmail.com>
2019-07-08 16:30:06 +0100

    corrected typos in create-exercise.component.js

--- MERN/mern-exercise-tracker/src/components/create-exercise.component.js
+++ MERN/mern-exercise-tracker/src/components/create-exercise.component.js
@@ -83,42 +83,47 @@ export default class CreateExercise extends Component {
                 return (
                   <option key={user} value={user}>
                     {user}
-                  </option>;
+                  </option>
                 );
               })}
             </select>
-                </div>
-                <div className="form-group">
-                    <label>Description: j</label>
-                    <input type='text'
-                        required
-                        className='form-control'
-                        value={this.state.description}
-                        onChange={this.onChangeDescription}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Duration (in minutes): </label>
-                    <input 
-                        type='text'
-                        className='form-control'
-                        value={this.state.duration}
-                        onChange={this.onChangeDuration}
-                        />
-                </div>
-                <div className="form-group">
-                    <label>Date: </label>
-                    <div>
-                        <DatePicker
-                            selected={this.state.date}
-                            onChange={this.onChangeDate}
-                        />
-                    </div>
-                </div>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              required
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (in minutes): </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
 
-                <div className="form-group">
-                    <input type="submit" value='Create Exercise Log' className='btn btn-primary' />
-                </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise Log"
+              className="btn btn-primary"
+            />
+          </div>
         </form>
       </div>
     );
