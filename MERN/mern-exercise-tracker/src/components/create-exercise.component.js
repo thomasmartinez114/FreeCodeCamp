4d3d2dcbf3b030a7d81d3bda2e0cbb6c8b6c9c63
unknown <tmartinez114@gmail.com>
2019-07-08 15:21:22 -0400

    imported DatePicker and the styling for it

--- MERN/mern-exercise-tracker/src/components/create-exercise.component.js
+++ MERN/mern-exercise-tracker/src/components/create-exercise.component.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
   constructor(props) {
