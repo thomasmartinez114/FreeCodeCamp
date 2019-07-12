7264d5a74fdae3104e9e2167110016bbcb525526
tmartinez114 <tmartinez114@gmail.com>
2019-07-12 15:19:45 -0400

    added constructor this.deleteExercise & componentDidMount() to exercises-list.component.js

--- MERN/mern-exercise-tracker/src/components/exercises-list.component.js
+++ MERN/mern-exercise-tracker/src/components/exercises-list.component.js
@@ -3,6 +3,25 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = { exercises: [] };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/exercises/")
+      .then(response => {
+        this.setState({ exercises: response.data });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return (
       <div>
