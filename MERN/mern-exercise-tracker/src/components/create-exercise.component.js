03ec71d0731bf090e5b5729b9a5552a6d0b71e41
tmartinez114 <tmartinez114@gmail.com>
2019-07-12 12:07:09 -0400

    componentDidMount with axios to pull the username from DB

--- MERN/mern-exercise-tracker/src/components/create-exercise.component.js
+++ MERN/mern-exercise-tracker/src/components/create-exercise.component.js
@@ -23,10 +23,15 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ["test user"],
-      username: "test user"
-    });
+    axios.get('http://localhost:5000/users/')
+      .then(response => (
+      if (response.data.length > 0) {
+        this.setState({
+          users: response.data.map(user => user.username),
+          username: response.data[0].username
+        });
+      }
+    ))
   }
 
   onChangeUsername(e) {
