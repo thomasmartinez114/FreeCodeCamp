8fd744c4ddc25ffaab026b547f59e0503eee05d0
tmartinez114 <tmartinez114@gmail.com>
2019-07-12 15:24:23 -0400

    added deleteExercises(id)

--- MERN/mern-exercise-tracker/src/components/exercises-list.component.js
+++ MERN/mern-exercise-tracker/src/components/exercises-list.component.js
@@ -22,6 +22,15 @@ export default class ExercisesList extends Component {
       });
   }
 
+  deleteExercise(id) {
+    axios
+      .delete("http://localhost:5000/exercises/" + id)
+      .then(res => console.log(res.data));
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    });
+  }
+
   render() {
     return (
       <div>
