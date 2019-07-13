622d54993af66b37d0db81d6d9432363c4c1d0ad
tmartinez114 <tmartinez114@gmail.com>
2019-07-12 21:01:13 -0400

    added exercise component within exercises-list and added delete button

--- MERN/mern-exercise-tracker/src/components/exercises-list.component.js
+++ MERN/mern-exercise-tracker/src/components/exercises-list.component.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit/" + props.exercise._id}>edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        delete
+      </a>
+    </td>
+  </tr>
+);
+
 export default class ExercisesList extends Component {
   constructor(props) {
     super(props);
@@ -35,15 +55,21 @@ export default class ExercisesList extends Component {
 
   exerciseList() {
     return this.state.exercises.map(currentexercise => {
-      return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id} />;
-    })
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
   }
 
   render() {
     return (
       <div>
         <h3>Logged Exercises</h3>
-        <table className='table'>
+        <table className="table">
           <thead className="thead-lig">
             <tr>
               <th>Username</th>
@@ -53,12 +79,9 @@ export default class ExercisesList extends Component {
               <th>Actions</th>
             </tr>
           </thead>
-          <tbody>
-            { this.exerciseList() }
-          </tbody>
+          <tbody>{this.exerciseList()}</tbody>
         </table>
-  </div>
-)
+      </div>
     );
   }
 }
