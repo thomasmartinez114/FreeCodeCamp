67ce0abee24c8387bf5150053643da9ad313e9ff
tmartinez114 <tmartinez114@gmail.com>
2019-07-23 18:44:45 -0400

    completed 52-55

---
+++ javascript/01 Basic Javascript/54stand-in-line.js
@@ -0,0 +1,15 @@
+function nextInLine(arr, item) {
+  // Your code here
+
+  arr.push(item);
+  var removed = arr.shift();
+  return removed;
+}
+
+// Test Setup
+var testArr = [1, 2, 3, 4, 5];
+
+// Display Code
+console.log('Before: ' + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log('After: ' + JSON.stringify(testArr));
