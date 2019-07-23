55fb3b1b6c0b9a5ad22f381f6d591c1ef9f71f20
tmartinez114 <tmartinez114@gmail.com>
2019-07-23 16:23:27 -0400

    completed 50 and 51

---
+++ javascript/01 Basic Javascript/51return-a-value-from-a-function-with-return.js
@@ -0,0 +1,12 @@
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(number) {
+  return number * 5;
+}
+
+console.log(minusSeven(10));
+console.log(timesFive(10));
