7eef93706fd2f398165648a2218ac6aa469e4c87
tmartinez114 <tmartinez114@gmail.com>
2019-07-26 17:09:11 -0400

    completed 73-76

---
+++ javascript/01 Basic Javascript/73selecting-from-many-options-with-switch-statements.js
@@ -0,0 +1,27 @@
+function caseInSwitch(val) {
+  var answer = '';
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return 'alpha';
+      break;
+    case 2:
+      return 'beta';
+      break;
+    case 3:
+      return 'gamma';
+      break;
+    case 4:
+      return 'delta';
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+console.log(caseInSwitch(1));
+console.log(caseInSwitch(2));
+console.log(caseInSwitch(3));
+console.log(caseInSwitch(4));
