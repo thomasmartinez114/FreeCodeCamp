47514f828d62aefb13bfa9e4bb1148d89764128c
tmartinez114 <tmartinez114@gmail.com>
2019-07-28 11:55:00 -0400

    completed 88 and 89

---
+++ javascript/01 Basic Javascript/89manipulating-complex-objects.js
@@ -0,0 +1,18 @@
+var myMusic = [
+  {
+    artist: 'Billy Joel',
+    title: 'Piano Man',
+    release_year: 1973,
+    formats: ['CD', '8T', 'LP'],
+    gold: true
+  },
+  {
+    artist: 'The Notorious B.I.G.',
+    title: 'Life After Death [Dual Disc]',
+    release_year: 1997,
+    formats: ['CD', 'Cassette', 'Vinyl']
+  }
+  // Add record here
+];
+
+console.log(myMusic[1].artist);
