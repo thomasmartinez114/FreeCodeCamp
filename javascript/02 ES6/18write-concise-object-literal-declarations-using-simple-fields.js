c8935d4752582ff04a1b001fcb0c3921a54b3f67
tmartinez114 <tmartinez114@gmail.com>
2019-08-13 17:59:30 -0400

    completed 18

---
+++ javascript/02 ES6/18write-concise-object-literal-declarations-using-simple-fields.js
@@ -0,0 +1,7 @@
+const createPerson = (name, age, gender) => {
+  'use strict';
+  // change code below this line
+  ({ name, age, gender });
+  // change code above this line
+};
+console.log(createPerson('Zodiac Hasbro', 56, 'male')); // returns a proper object
