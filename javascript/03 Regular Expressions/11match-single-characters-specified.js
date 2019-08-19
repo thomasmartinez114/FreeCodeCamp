72069208418e074291ca203d2a2600e4eabc95e5
tmartinez114 <tmartinez114@gmail.com>
2019-08-19 11:57:08 -0400

    updated number range

--- javascript/03 Regular Expressions/11match-single-characters-specified.js
+++ javascript/03 Regular Expressions/11match-single-characters-specified.js
@@ -1,3 +1,3 @@
 let quoteSample = '3 blind mice.';
-let myRegex = /[^aeious0-9]/gi; // Change this line
+let myRegex = /[^aeious0-99]/gi; // Change this line
 let result = quoteSample.match(myRegex); // Change this line
