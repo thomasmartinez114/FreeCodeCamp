8ea3aab3cc9929ea1e41a16da93fcfb17aab065c
tmartinez114 <tmartinez114@gmail.com>
2019-08-26 17:07:55 -0400

    added new files

--- javascript/05 Basic Data Structures/01arrayToStore.js
+++
@@ -1,3 +0,0 @@
-// Basic Data Structures: Use an Array to Store a Collection of Data
-
-let yourArray = ['Thomas', 19, 'Martin', true, 92]; // change this line
---
+++ javascript/05 Basic Data Structures/01use-an-array-to-store-a-collection-of-data.js
@@ -0,0 +1,3 @@
+let yourArray = ['Thomas', 19, false, 'Martinez', true]; // change this line
+
+console.log(yourArray);
