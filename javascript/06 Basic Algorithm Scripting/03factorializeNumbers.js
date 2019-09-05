f379144e02186830a5d2d71d893aa77150143c0f
tmartinez114 <tmartinez114@gmail.com>
2019-09-05 19:15:40 -0400

    completed 03

--- javascript/06 Basic Algorithm Scripting/03factorializeNumbers.js
+++ javascript/06 Basic Algorithm Scripting/03factorializeNumbers.js
@@ -1,10 +1,10 @@
 function factorialize(num) {
   var product = 1;
-  
-  for (var i = 1; i <= num; i++){
-      product = product * i;
+
+  for (var i = 1; i <= num; i++) {
+    product *= i;
   }
-  
+
   return product;
 }
 
