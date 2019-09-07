cd79fe7a5319680b3b7b703fc71165693e6fc371
tmartinez114 <tmartinez114@gmail.com>
2019-09-07 12:38:51 -0400

    completed 05

--- javascript/06 Basic Algorithm Scripting/05returnLargestNumbers.js
+++ javascript/06 Basic Algorithm Scripting/05returnLargestNumbers.js
@@ -8,11 +8,33 @@ function largestOfFour(arr) {
   // You can do this!
   var result = [];
 
-  for (var i = 0; i < arr.length; i++){
+  for (var i = 0; i < arr.length; i++) {
     // console.log(Math.max(...arr[i]))
-    result.push(Math.max(...arr[i]))
+    result.push(Math.max(...arr[i]));
   }
-  return result
+  return result;
 }
 
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+  [4, 5, 1, 3],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1]
+]);
+
+// Second version
+function largestOfFour(arr) {
+  var results = [];
+  for (var n = 0; n < arr.length; n++) {
+    var largestNumber = arr[n][0];
+    for (var sb = 1; sb < arr[n].length; sb++) {
+      if (arr[n][sb] > largestNumber) {
+        largestNumber = arr[n][sb];
+      }
+    }
+
+    results[n] = largestNumber;
+  }
+
+  return results;
+}
