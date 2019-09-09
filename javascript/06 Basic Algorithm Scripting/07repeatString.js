87b25081aaf32f7990eab3ec44e4f5458686a286
tmartinez114 <tmartinez114@gmail.com>
2019-09-09 18:29:53 -0400

    completed 07

--- javascript/06 Basic Algorithm Scripting/07repeatString.js
+++ javascript/06 Basic Algorithm Scripting/07repeatString.js
@@ -1,10 +1,6 @@
-function repeatStringNumTimes(str, num) {
-  // repeat after me
-  if (num >= 1) {
-    return str.repeat(num);
-  } else {
-    return "";
-  }
+function repeatStringNumTimes(string, times) {
+  if (times < 0) return '';
+  if (times === 1) return string;
+  else return string + repeatStringNumTimes(string, times - 1);
 }
-
-repeatStringNumTimes("abc", 3);
+repeatStringNumTimes('abc', 3);
