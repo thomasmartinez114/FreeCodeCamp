f03d18d5c9145aa05ad06388b358cd83f29e91bb
tmartinez114 <tmartinez114@gmail.com>
2019-09-10 20:22:28 -0400

    need to finish

--- javascript/06 Basic Algorithm Scripting/12sliceAndSplice.js
+++ javascript/06 Basic Algorithm Scripting/12sliceAndSplice.js
@@ -1,9 +1,7 @@
 function frankenSplice(arr1, arr2, n) {
-  var arr2Copy = arr2.slice();
-  for (var i = 0; i < arr1.length; i++) {
-    arr2Copy.splice(n++, 0, arr1[i]);
-  }
-  return arr2Copy;
+  // It's alive. It's alive!
+  var newArr = arr2.splice(0);
+  return newArr;
 }
 
-frankenSplice([1, 2, 3], [4, 5, 6], 1);
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
