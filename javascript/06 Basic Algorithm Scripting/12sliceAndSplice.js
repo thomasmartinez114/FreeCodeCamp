8e35c6e0191d25010dd6b66b0cbce024bd147151
tmartinez114 <tmartinez114@gmail.com>
2019-09-11 16:52:19 -0400

    completed 12

--- javascript/06 Basic Algorithm Scripting/12sliceAndSplice.js
+++ javascript/06 Basic Algorithm Scripting/12sliceAndSplice.js
@@ -1,6 +1,12 @@
 function frankenSplice(arr1, arr2, n) {
   // It's alive. It's alive!
   var newArr = arr2.slice();
+
+  for (var i = 0; i < arr1.length; i++) {
+    newArr.splice(n, 0, arr1[i]);
+    n++;
+  }
+
   return newArr;
 }
 
